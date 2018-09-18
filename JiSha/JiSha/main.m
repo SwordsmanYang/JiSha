55996f30178773929fb20c9f04e87e6f270df163
qing <yangchangqing@icarbonx.com>
2018-09-18 20:21:03 +0800

    第一次提交

---
+++ JiSha/JiSha/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JiSha
+//
+//  Created by yangcq on 2018/9/18.
+//  Copyright © 2018年 yangchangqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
